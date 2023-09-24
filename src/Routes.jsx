fd7e1a53209938c7a39de8e89b475c8486414f53
wukufy <cheviotin200@gmail.com>
2023-09-23 20:57:39 -0500

    combinacion de ramas

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,24 +3,27 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/patient/Home';
 import HistorialMedico from './pages/patient/HistorialMedico';
 import Pqrs from './pages/patient/Pqrs';
-import Navbar from './components/Navbar';
-import EjemploPage from './pages/admin/EjemploPage'; // Asegúrate de que la ruta sea correcta según tu estructura de carpetas
+import Navbar from './components/patient/Navbar';
+import HomeAdmin from './pages/admin/HomeAdmin'; // Asegúrate de que la ruta sea correcta según tu estructura de carpetas
+import CreateMedico from './pages/admin/CreateMedico'; // Asegúrate de que la ruta sea correcta según tu estructura de carpetas
+import PqrsAdmin from './pages/admin/PqrsAdmin'; // Asegúrate de que la ruta sea correcta según tu estructura de carpetas
+import HistorialConsultasMed from './pages/admin/HistorialConsultasMed'; // Asegúrate de que la ruta sea correcta según tu estructura de carpetas
 
 const AppRoutes = () => {
   return (
     <Router>
-      <Navbar /> 
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/historialMedico" element={<HistorialMedico />} />
-        <Route path="/pqrs" element={<Pqrs />} /> 
-        <Route path="/homeAdmin" element={<HomeAdmin />} /> 
+        <Route path="/pqrs" element={<Pqrs />} />
+        <Route path="/homeAdmin" element={<HomeAdmin />} />
         <Route path="/createmedico" element={<CreateMedico />} />
         <Route path="/pqrsadmin" element={<PqrsAdmin />} />
         <Route path="/historialconsultasmed" element={<HistorialConsultasMed />} />
       </Routes>
-    </Router> 
-  ); 
+    </Router>
+  );
 }
 
 export default AppRoutes;
