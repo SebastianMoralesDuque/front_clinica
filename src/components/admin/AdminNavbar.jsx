6c30258f1d3dcf15807cf8d66d3065f1fa0955db
sebas <sebas@sebas.sebas>
2023-10-05 15:32:07 -0500

    se arregla el navbar de administrador

--- src/components/admin/AdminNavbar.jsx
+++ src/components/admin/AdminNavbar.jsx
@@ -21,16 +21,17 @@ const Navbar = () => {
   
 
   // Obtén la información del administrador desde el localStorage
-  const adminData = JSON.parse(localStorage.getItem('adminData'));
+  const adminData = JSON.parse(localStorage.getItem('userData'));
   const adminEmail = adminData ? adminData.adminData.email : '';
-
   // Obtén el nombre del administrador hasta el "@" (correo sin el dominio)
   const adminName = adminEmail ? adminEmail.split('@')[0] : '';
 
   return (
     <nav className="bg-gray-800 py-4 px-6 flex justify-between items-center">
       <div>
-        <Link to="/" className="text-white text-2xl font-semibold">Clínica</Link>
+        <Link to="/" className="text-white text-2xl font-semibold">
+          Clínica
+        </Link>
       </div>
       <ul className="flex space-x-4 items-center">
         <li>
@@ -38,30 +39,20 @@ const Navbar = () => {
             <span className="text-white">{`¡Bienvenido, ${adminName}!`}</span>
           )}
         </li>
-        <li>
-          {adminData && (
+        {adminData && (
+          <li>
             <button
               className="text-white bg-red-500 hover:bg-red-600 px-4 py-2 rounded-full transition duration-300"
               onClick={handleLogout}
             >
               Cerrar Sesión
             </button>
-          )}
-        </li>
-        <li>
-          {!adminData && (
-            <button
-              className="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-full transition duration-300"
-              onClick={openLoginModal}
-            >
-              Iniciar Sesión de admin
-            </button>
-          )}
-        </li>
+          </li>
+        )}
       </ul>
       {isLoginModalOpen && <LoginModal closeModal={closeModal} />}
     </nav>
   );
-}
+};
 
 export default Navbar;
