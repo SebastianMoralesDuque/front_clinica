676bebb5b7bdaef238dbd414cc5205808c05b967
sebas <sebas@sebas.sebas>
2023-10-05 13:19:58 -0500

    se corrije el servicio de login para todos los usuarios

--- src/services/userLoginService.jsx
+++ src/services/userLoginService.jsx
@@ -26,7 +26,7 @@ const loginUser = async (formData) => {
 
 const loginAdmin = async (formData) => {
   try {
-    const response = await fetch(url, {
+    const response = await fetch('http://localhost:9009/usuarios/gestion/login/admin', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -37,37 +37,51 @@ const loginAdmin = async (formData) => {
     if (response.ok) {
       const userData = await response.json();
 
-      localStorage.setItem('userType', userType);
-      localStorage.setItem(storageKey, JSON.stringify(userData));
+      // Almacenar datos en el localStorage para el tipo "paciente"
+      localStorage.setItem('userType', 'admin');
+      localStorage.setItem('userData', JSON.stringify(userData));
 
       return true;
     } else {
       return false;
     }
   } catch (error) {
-    console.error(`Error al iniciar sesión como ${userType}:`, error);
+    console.error('Error al iniciar sesión como administrador:', error);
+    return false;
+  }
+};
+
+const loginDoctor = async (formData) => {
+  try {
+    const response = await fetch('http://localhost:9009/usuarios/gestion/login/medico', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      const medicoData = await response.json();
+
+      // Almacenar datos en el localStorage para el tipo "médico"
+      localStorage.setItem('medicoType', 'medico');
+      localStorage.setItem('medicoData', JSON.stringify(medicoData));
+
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error('Error al iniciar sesión como médico:', error);
     return false;
   }
 };
 
 const UserService = {
-  loginUser: async (formData) => {
-    const url = 'http://localhost:9009/usuarios/gestion/login/paciente';
-    const storageKey = 'userData';
-    return await login(url, formData, 'paciente', storageKey);
-  },
-
-  loginAdmin: async (formData) => {
-    const url = 'http://localhost:9009/usuarios/gestion/login/admin';
-    const storageKey = 'adminData';
-    return await login(url, formData, 'admin', storageKey);
-  },
-
-  loginDoctor: async (formData) => {
-    const url = 'http://localhost:9009/usuarios/gestion/login/medico';
-    const storageKey = 'medicoData';
-    return await login(url, formData, 'medico', storageKey);
-  },
+  loginUser,
+  loginAdmin,
+  loginDoctor,
 };
 
 export default UserService;
